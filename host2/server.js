c00a7384890f2c6d8181fc2f00cd3962daf09ac4
hhhgll <73424312+hhhgll@users.noreply.github.com>
2021-11-23 16:15:56 +0800

    Update server.js

--- host2/server.js
+++ host2/server.js
@@ -16,11 +16,11 @@ app.get('/api/date', (req, res) => {
 
 app.get('/api/datecors', (req, res) => {
     res.set('Access-Control-Allow-Origin', 'http://web.cybersecurity.seu.edu')
-    res.send({ datecors: Date.now() })
+    res.send({ date: Date.now() })
 })
 
 app.get('/api/jsondate', (req, res) => {
     let callback = req.query.callback;
-    let Str = `${callback}(${JSON.stringify({ datejson: Date.now() })})`;
+    let Str = `${callback}(${JSON.stringify({ date: Date.now() })})`;
     res.send(Str);
 })
